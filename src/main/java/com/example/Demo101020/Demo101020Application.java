e9381cc146495c17882a81da3dbe5ab08be8033e
lopamudrasatpathy <lopamudra.satpathy@gmail.com>
2020-10-10 14:16:03 -0500

    Initial version

---
+++ src/main/java/com/example/Demo101020/Demo101020Application.java
@@ -0,0 +1,13 @@
+package com.example.Demo101020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo101020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo101020Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Demo101020/Demo101020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Demo101020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo101020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
